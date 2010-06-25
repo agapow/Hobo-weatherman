c688a9b00c7e1eb4f00b4c6b52ba5827e04be745
Paul-Michael Agapow <github@agapow.net>
2010-06-25 16:36:01 +0100

    playing with perms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,28 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+	
+	def allow_write_global
+		acting_user.administrator?
+	end
+	
+	def allow_write_collection
+		allow_write_global
+	end
+	
+	def allow_write_record
+		allow_write_global
+	end
+
+	def allow_read_global
+		true
+	end
+	
+	def allow_read_collection(field)
+		allow_read_global
+	end
+	
+	def allow_read_record(field)
+		allow_read_global
+	end
+
 end
